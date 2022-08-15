aea95841f7efbe037378371781a9e2d91e7f039a
shengchenyang <15538221825@163.com>
2022-08-15 17:36:02 +0800

    DemoSpider 项目 - 优化 demo_five 的异步存储场景

--- DemoSpider/spiders/demo_five.py
+++ DemoSpider/spiders/demo_five.py
@@ -1,4 +1,3 @@
-import json
 import pandas
 from loguru import logger
 from scrapy.http import Request
@@ -30,14 +29,9 @@ class DemoFiveSpider(AyuSpider):
     custom_settings = {
         # 数据表的前缀名称，用于标记属于哪个项目
         'MYSQL_TABLE_PREFIX': "demo_",
-        # mysql db 连接池配置
-        'POOL_DB_CONFIG': {
-            'maxconnections': 5,
-            'blocking': True,
-        },
         'ITEM_PIPELINES': {
             # 激活此项则数据会存储至 Mysql
-            'ayugespidertools.Pipelines.AyuTurboMysqlPipeline': 300,
+            'ayugespidertools.Pipelines.AyuTwistedMysqlPipeline': 300,
         },
 
         'DOWNLOADER_MIDDLEWARES': {
@@ -45,7 +39,7 @@ class DemoFiveSpider(AyuSpider):
             'ayugespidertools.Middlewares.RandomRequestUaMiddleware': 400,
         },
 
-        'CONCURRENT_REQUESTS': 100,
+        'CONCURRENT_REQUESTS': 64,
         'DOWNLOAD_DELAY': 0.1
     }
 
@@ -80,48 +74,24 @@ class DemoFiveSpider(AyuSpider):
             BookInfoItem['alldata'] = Book_Info
             BookInfoItem['table'] = Table_Enum.book_info_list_table.value['value']
             # logger.info(f"BookInfoItem: {BookInfoItem}")
-            yield BookInfoItem
+            # yield BookInfoItem
 
-        # data_list = json.loads(response.text)['data']
-        # for curr_data in data_list:
-        #     article_detail_url = curr_data['articleDetailUrl']
-        #     article_title = curr_data['articleTitle']
-        #     comment_count = curr_data['commentCount']
-        #     favor_count = curr_data['favorCount']
-        #     nick_name = curr_data['nickName']
-        #     logger.info(f"article data: {article_detail_url, article_title, comment_count, favor_count, nick_name}")
-        #
-        #     Aritle_Info = dict()
-        #     Aritle_Info['article_detail_url'] = {'key_value': article_detail_url, 'notes': '文章详情链接'}
-        #     Aritle_Info['article_title'] = {'key_value': article_title, 'notes': '文章标题'}
-        #     Aritle_Info['comment_count'] = {'key_value': comment_count, 'notes': '文章评论数量'}
-        #     Aritle_Info['favor_count'] = {'key_value': favor_count, 'notes': '文章收藏数量'}
-        #     Aritle_Info['nick_name'] = {'key_value': nick_name, 'notes': '文章作者昵称'}
-        #
-        #     AritleInfoItem = DataItem()
-        #     AritleInfoItem['alldata'] = Aritle_Info
-        #     AritleInfoItem['table'] = Table_Enum.aritle_list_table.value['value']
-        #     logger.info(f"AritleInfoItem: {AritleInfoItem}")
-        #     # yield AritleInfoItem
-        #
-        #     # 数据入库逻辑
-        #     try:
-        #         # 测试 mysql_engine 的去重功能
-        #         sql = '''select `id` from `{}` where `article_detail_url` = "{}" limit 1'''.format(
-        #             self.custom_settings['MYSQL_TABLE_PREFIX'] + Table_Enum.aritle_list_table.value['value'],
-        #             article_detail_url)
-        #         df = pandas.read_sql(sql, self.mysql_engine)
-        #
-        #         # 如果为空，说明此数据不存在于数据库，则新增
-        #         if df.empty:
-        #             yield AritleInfoItem
-        #
-        #         # 如果已存在，1). 若需要更新，请自定义更新数据结构和更新逻辑；2). 若不用更新，则跳过即可。
-        #         else:
-        #             logger.debug(f"标题为 ”{article_title}“ 的数据已存在，请自定义更新逻辑")
-        #
-        #     except Exception as e:
-        #         if any(["1146" in str(e), "doesn't exist" in str(e)]):
-        #             yield AritleInfoItem
-        #         else:
-        #             logger.error("请查看数据库链接或网络是否通畅！")
+            # 数据入库逻辑
+            try:
+                # 测试 mysql_engine 的去重功能
+                sql = '''select `id` from `{}` where `book_name` = "{}" limit 1'''.format(self.custom_settings['MYSQL_TABLE_PREFIX'] + Table_Enum.book_info_list_table.value['value'], book_name)
+                df = pandas.read_sql(sql, self.mysql_engine)
+
+                # 如果为空，说明此数据不存在于数据库，则新增
+                if df.empty:
+                    yield BookInfoItem
+
+                # 如果已存在，1). 若需要更新，请自定义更新数据结构和更新逻辑；2). 若不用更新，则跳过即可。
+                else:
+                    logger.debug(f"标题为 ”{book_name}“ 的数据已存在，请自定义更新逻辑")
+
+            except Exception as e:
+                if any(["1146" in str(e), "doesn't exist" in str(e)]):
+                    yield BookInfoItem
+                else:
+                    logger.error("请查看数据库链接或网络是否通畅！")
