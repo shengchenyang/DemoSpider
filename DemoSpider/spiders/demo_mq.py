13e58d0e616a20ebf2f4c5bbaf24b53ee0456a02
shengchenyang <15538221825@163.com>
2023-05-29 15:13:56 +0800

    更新示例

---
+++ DemoSpider/spiders/demo_mq.py
@@ -0,0 +1,83 @@
+from ayugespidertools.common.utils import ToolsForAyu
+from ayugespidertools.spiders import AyuSpider
+from scrapy.http import Request
+from scrapy.http.response.text import TextResponse
+
+from DemoSpider.items import TableEnum
+
+"""
+####################################################################################################
+# collection_website: CSDN - 专业开发者社区
+# collection_content: 热榜文章排名 Demo 采集示例 - 内容推送到 RabbitMQ
+# create_time: 2023-05-26
+# explain:
+# demand_code_prefix = ""
+####################################################################################################
+"""
+
+
+class DemoMqSpider(AyuSpider):
+    name = "demo_mq"
+    allowed_domains = ["blog.csdn.net"]
+    start_urls = ["https://blog.csdn.net/"]
+
+    custom_settings = {
+        "ITEM_PIPELINES": {
+            # 激活此项则数据会存储至 Mysql
+            "ayugespidertools.pipelines.AyuMQPipeline": 300,
+        },
+    }
+
+    def start_requests(self):
+        """
+        get 请求首页，获取项目列表数据
+        """
+        yield Request(
+            url="https://blog.csdn.net/phoenix/web/blog/hot-rank?page=0&pageSize=25&type=",
+            callback=self.parse_first,
+            headers={
+                "referer": "https://blog.csdn.net/rank/list",
+            },
+            cb_kwargs={
+                "curr_site": "csdn",
+            },
+            dont_filter=True,
+        )
+
+    def parse_first(self, response: TextResponse, curr_site: str):
+        # 日志使用: scrapy 的 self.logger 或本库的 self.slog 或直接使用全局的 logger handle 也行（根据场景自行选择）
+        self.slog.info(f"当前采集的站点为: {curr_site}")
+
+        # 你可以自定义解析规则，使用 lxml 还是 response.css response.xpath 等等都可以。
+        data_list = ToolsForAyu.extract_with_json(
+            json_data=response.json(), query="data"
+        )
+        for curr_data in data_list:
+            article_detail_url = ToolsForAyu.extract_with_json(
+                json_data=curr_data, query="articleDetailUrl"
+            )
+
+            article_title = ToolsForAyu.extract_with_json(
+                json_data=curr_data, query="articleTitle"
+            )
+
+            comment_count = ToolsForAyu.extract_with_json(
+                json_data=curr_data, query="commentCount"
+            )
+
+            favor_count = ToolsForAyu.extract_with_json(
+                json_data=curr_data, query="favorCount"
+            )
+
+            nick_name = ToolsForAyu.extract_with_json(
+                json_data=curr_data, query="nickName"
+            )
+
+            yield {
+                "article_detail_url": article_detail_url,
+                "article_title": article_title,
+                "comment_count": comment_count,
+                "favor_count": favor_count,
+                "nick_name": nick_name,
+                "_table": TableEnum.article_list_table.value["value"],
+            }
