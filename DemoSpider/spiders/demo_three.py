56fe4bd25f52733aaf33e4756ffaf76247a41883
shengchenyang <15538221825@163.com>
2023-12-03 17:14:52 +0800

    update demo_three example

--- DemoSpider/spiders/demo_three.py
+++ DemoSpider/spiders/demo_three.py
@@ -6,7 +6,6 @@ from ayugespidertools.items import AyuItem, DataItem
 from ayugespidertools.spiders import AyuSpider
 from loguru import logger
 from scrapy.http import Request
-from sqlalchemy import text
 
 
 class DemoThreeSpider(AyuSpider):
@@ -14,8 +13,6 @@ class DemoThreeSpider(AyuSpider):
     allowed_domains = ["blog.csdn.net"]
     start_urls = ["https://blog.csdn.net/"]
     custom_settings = {
-        # 打开 mysql 引擎开关，用于数据入库前更新逻辑判断
-        "MYSQL_ENGINE_ENABLED": True,
         # 开启远程配置服务(优先级 consul > nacos)
         "APP_CONF_MANAGE": True,
         "ITEM_PIPELINES": {
@@ -75,20 +72,4 @@ class DemoThreeSpider(AyuSpider):
                 _table=DataItem(_save_table, "项目列表信息"),
             )
             self.slog.info(f"ArticleInfoItem: {ArticleInfoItem}")
-            # yield ArticleInfoItem
-
-            try:
-                _sql = text(
-                    f"""select `id` from `{_save_table}` where `article_detail_url` = "{article_detail_url}" limit 1"""
-                )
-                result = self.mysql_engine.execute(_sql).fetchone()
-                if not result:
-                    yield ArticleInfoItem
-                else:
-                    self.slog.debug(f'标题为 "{article_title}" 的数据已存在')
-            except Exception as e:
-                # 若数据库或数据表不存在时，直接返回 item 即可，会自动创建所依赖的数据库数据表及字段注释（前提是用户有对应权限）
-                if any(["1146" in str(e), "1054" in str(e), "doesn't exist" in str(e)]):
-                    yield ArticleInfoItem
-                else:
-                    raise ValueError(f"请查看网络是否通畅，或 sql 是否正确！Error: {e}") from e
+            yield ArticleInfoItem
