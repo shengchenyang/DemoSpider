277ff4d082451f696ce9c8db586b3574b1de3800
shengchenyang <15538221825@163.com>
2023-12-19 09:25:18 +0800

    feat: add demo_oracle_twisted

---
+++ DemoSpider/spiders/demo_oracle_twisted.py
@@ -0,0 +1,58 @@
+# oracle 场景不会添加自动创建数据库，表及字段等功能，请手动管理
+from ayugespidertools.common.utils import ToolsForAyu
+from ayugespidertools.items import AyuItem, DataItem
+from ayugespidertools.spiders import AyuSpider
+from scrapy.http import Request
+from scrapy.http.response.text import TextResponse
+
+
+class DemoOracleTwistedSpider(AyuSpider):
+    name = "demo_oracle_twisted"
+    allowed_domains = ["faloo.com"]
+    start_urls = ["http://b.faloo.com/"]
+    custom_settings = {
+        "DATABASE_ENGINE_ENABLED": True,
+        "ITEM_PIPELINES": {
+            "ayugespidertools.pipelines.AyuTwistedOraclePipeline": 300,
+        },
+        "CONCURRENT_REQUESTS": 64,
+        "DOWNLOAD_DELAY": 0.01,
+    }
+
+    def start_requests(self):
+        """请求首页，获取项目列表数据"""
+        for page in range(1, 11):
+            url = f"https://b.faloo.com/y_0_0_0_0_3_15_{page}.html"
+            yield Request(url=url, callback=self.parse_first, dont_filter=True)
+
+    def parse_first(self, response: TextResponse):
+        book_info_list = ToolsForAyu.extract_with_xpath(
+            response=response,
+            query='//div[@class="TwoBox02_01"]/div',
+            return_selector=True,
+        )
+
+        for book_info in book_info_list:
+            book_name = ToolsForAyu.extract_with_xpath(
+                response=book_info, query="div[2]//h1/@title"
+            )
+
+            book_href = ToolsForAyu.extract_with_xpath(
+                response=book_info, query="div[2]//h1/a/@href"
+            )
+            book_href = response.urljoin(book_href)
+
+            book_intro = ToolsForAyu.extract_with_xpath(
+                response=book_info, query='div[2]/div[@class="TwoBox02_06"]/a/text()'
+            )
+
+            _save_table = "demo_oracle_twisted"
+            BookInfoItem = AyuItem(
+                book_name=book_name,
+                book_href=book_href,
+                book_intro=book_intro,
+                _table=_save_table,
+            )
+
+            self.slog.info(f"BookInfoItem: {BookInfoItem}")
+            yield BookInfoItem
