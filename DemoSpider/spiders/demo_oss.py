50a9dd4e4ba43c768ac2e47fe758d649a112a890
shengchenyang <15538221825@163.com>
2024-03-10 13:49:42 +0800

    chore: update demo_oss

--- DemoSpider/spiders/demo_oss.py
+++ DemoSpider/spiders/demo_oss.py
@@ -1,14 +1,18 @@
 """
 Note:
-    上传 AyuItem 中的部分资源字段上传到 oss 的示例一：
-    这是使用 AyuAsyncOssPipeline 根据 [oss:ali] 中的规则配置来上传 AyuItem
-部分资源字段到 oss 的示例，规则配置如下：
-    参数 upload_fields_suffix 为需要上传的字段规则，如果此字段包含此后缀则会上传此网络流至 oss，默认为 _file_url；
-    参数 oss_fields_prefix 为上传至 oss 的字段对应的新 AyuItem 字段名规则，新字段会在原字段加上此前缀，默认为 _。
+    将 AyuItem 中的部分字段上传到 oss 的示例一：
+    - 这是使用 AyuAsyncOssPipeline 根据 [oss:ali] 中的规则限定来将 AyuItem 中符合规则的
+字段对应的资源上传到 oss 的示例：
+    - 规则配置如下:
+        - 参数 upload_fields_suffix 为需要上传的字段规则，如果此字段包含此后缀则会上传此网
+    络流至 oss，默认为 _file_url；
+        - 参数 oss_fields_prefix 为上传至 oss 的字段对应的新 AyuItem 字段名规则，新字段
+    会在原字段加上此前缀，默认为 _。
 
 NOTICE:
-    运行前需要安装 ayugespidertools[all]
-    虽然看上去方便了开发，但是规则限定这种方式不优雅，推荐查看 demo_oss_sec 的示例。
+    - 运行前需要安装 ayugespidertools[all]
+    - 虽然方便了开发，但规则限定这种方式不太优雅，不是所有人都喜欢。也可根据 demo_oss_sec 的
+示例自行实现。
 """
 
 import json
