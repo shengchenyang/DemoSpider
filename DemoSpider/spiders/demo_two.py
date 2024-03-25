39d47c4b2de1475ea46429e6a5ef1c790088116d
shengchenyang <15538221825@163.com>
2024-03-25 17:48:05 +0800

    chore: update demo_two

--- DemoSpider/spiders/demo_two.py
+++ DemoSpider/spiders/demo_two.py
@@ -1,18 +1,12 @@
 # 热榜文章排名 Demo 采集示例 - 存入 MongoDB (配置根据本地 .conf 取值)
 import json
-from typing import TYPE_CHECKING, Union
+from typing import Any
 
 from ayugespidertools.items import AyuItem
 from ayugespidertools.spiders import AyuSpider
 from scrapy.http import Request
 
-if TYPE_CHECKING:
-    from scrapy.http import Response
-    from scrapy.http.response.html import HtmlResponse
-    from scrapy.http.response.text import TextResponse
-    from scrapy.http.response.xml import XmlResponse
-
-    ScrapyResponse = Union[TextResponse, XmlResponse, HtmlResponse, Response]
+from DemoSpider.common.types import ScrapyResponse
 
 
 class DemoTwoSpider(AyuSpider):
@@ -30,7 +24,7 @@ class DemoTwoSpider(AyuSpider):
         },
     }
 
-    def start_requests(self):
+    def start_requests(self) -> Request:
         """
         get 请求首页，获取项目列表数据
         """
@@ -43,7 +37,7 @@ class DemoTwoSpider(AyuSpider):
             dont_filter=True,
         )
 
-    def parse_first(self, response: "ScrapyResponse"):
+    def parse_first(self, response: ScrapyResponse) -> Any:
         data_list = json.loads(response.text)["data"]
         for curr_data in data_list:
             article_detail_url = curr_data.get("articleDetailUrl")
