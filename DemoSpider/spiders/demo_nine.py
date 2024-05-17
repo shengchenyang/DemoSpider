75584e846cc8a386e36fa52a5c507eb3939637fe
shengchenyang <15538221825@163.com>
2024-05-17 19:04:18 +0800

    chore: update demo_nine

--- DemoSpider/spiders/demo_nine.py
+++ DemoSpider/spiders/demo_nine.py
@@ -11,66 +11,53 @@ from DemoSpider.common.types import ScrapyResponse
 
 class DemoNineSpider(AyuSpider):
     name = "demo_nine"
-    allowed_domains = ["csdn.net"]
-    start_urls = ["https://blog.csdn.net/"]
+    allowed_domains = ["readthedocs.io"]
+    start_urls = ["https://readthedocs.io"]
     custom_settings = {
         # 打开数据库引擎开关，用于数据入库前更新逻辑判断
         "DATABASE_ENGINE_ENABLED": True,
         "ITEM_PIPELINES": {
             "ayugespidertools.pipelines.AyuFtyPostgresPipeline": 300,
         },
-        "DOWNLOADER_MIDDLEWARES": {
-            "ayugespidertools.middlewares.RandomRequestUaMiddleware": 400,
-        },
     }
 
     def start_requests(self) -> Iterable[Request]:
         """get 请求首页，获取项目列表数据"""
         yield Request(
-            url="https://blog.csdn.net/phoenix/web/blog/hot-rank?page=0&pageSize=25&type=",
+            url="https://ayugespidertools.readthedocs.io/en/latest/",
             callback=self.parse_first,
-            headers={
-                "referer": "https://blog.csdn.net/rank/list",
-            },
         )
 
     def parse_first(self, response: ScrapyResponse) -> Any:
         _save_table = "demo_nine"
+        li_list = response.xpath('//div[@aria-label="Navigation menu"]/ul/li')
+        for curr_li in li_list:
+            octree_text = curr_li.xpath("a/text()").get()
+            octree_href = curr_li.xpath("a/@href").get()
 
-        data_list = json.loads(response.text)["data"]
-        for curr_data in data_list:
-            article_detail_url = curr_data.get("articleDetailUrl")
-            article_title = curr_data.get("articleTitle")
-            comment_count = curr_data.get("commentCount")
-            favor_count = curr_data.get("favorCount")
-            nick_name = curr_data.get("nickName")
-
-            article_item = AyuItem(
-                article_detail_url=article_detail_url,
-                article_title=article_title,
-                comment_count=comment_count,
-                favor_count=favor_count,
-                nick_name=nick_name,
+            octree_item = AyuItem(
+                octree_text=octree_text,
+                octree_href=octree_href,
                 _table=_save_table,
             )
-            self.slog.info(f"article_item: {article_item}")
-            # yield article_item
+            self.slog.info(f"octree_item: {octree_item}")
+            # yield octree_item
 
             # 同样也可使用之前的 pandas 结合对应的 <db>_engine 来去重，各有优缺点。
             # 也可自行实现，本库模版中使用 SQLAlchemy 结合对应 <db>_engine_conn 的方式实现。
             if self.postgres_engine_conn:
                 try:
                     _sql = text(
-                        f"""select id from {_save_table} where article_detail_url = '{article_detail_url}' limit 1"""
+                        f"""select id from {_save_table} where octree_text = '{octree_text}' limit 1"""
                     )
                     result = self.postgres_engine_conn.execute(_sql).fetchone()
                     if not result:
                         self.postgres_engine_conn.rollback()
-                        yield article_item
+                        yield octree_item
                     else:
-                        self.slog.debug(f'标题为 "{article_title}" 的数据已存在')
+                        self.slog.debug(f'标题为 "{octree_text}" 的数据已存在')
                 except Exception:
                     self.postgres_engine_conn.rollback()
-                    yield article_item
+                    yield octree_item
             else:
-                yield article_item
+                yield octree_item
