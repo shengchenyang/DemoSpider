13d23b7e09e8249decbdd8ea71d94a9f98ef02b0
shengchenyang <15538221825@163.com>
2024-05-17 19:18:07 +0800

    chore: update demo_four

--- DemoSpider/spiders/demo_four.py
+++ DemoSpider/spiders/demo_four.py
@@ -1,5 +1,4 @@
 # 存入 MongoDB 示例 (配置根据 consul 取值)
-import json
 from typing import Any, Iterable
 
 from ayugespidertools.items import AyuItem
@@ -11,8 +10,8 @@ from DemoSpider.common.types import ScrapyResponse
 
 class DemoFourSpider(AyuSpider):
     name = "demo_four"
-    allowed_domains = ["blog.csdn.net"]
-    start_urls = ["https://blog.csdn.net/"]
+    allowed_domains = ["readthedocs.io"]
+    start_urls = ["https://readthedocs.io"]
     custom_settings = {
         # 开启远程配置服务(优先级 consul > nacos)
         "APP_CONF_MANAGE": True,
@@ -20,41 +19,25 @@ class DemoFourSpider(AyuSpider):
             # 激活此项则数据会存储至 MongoDB
             "ayugespidertools.pipelines.AyuFtyMongoPipeline": 300,
         },
-        "DOWNLOADER_MIDDLEWARES": {
-            # 随机请求头
-            "ayugespidertools.middlewares.RandomRequestUaMiddleware": 400,
-        },
     }
 
     def start_requests(self) -> Iterable[Request]:
-        """
-        get 请求首页，获取项目列表数据
-        """
         yield Request(
-            url="https://blog.csdn.net/phoenix/web/blog/hot-rank?page=0&pageSize=25&type=",
+            url="https://ayugespidertools.readthedocs.io/en/latest/",
             callback=self.parse_first,
-            headers={
-                "referer": "https://blog.csdn.net/rank/list",
-            },
         )
 
     def parse_first(self, response: ScrapyResponse) -> Any:
-        data_list = json.loads(response.text)["data"]
-        for curr_data in data_list:
-            article_detail_url = curr_data.get("articleDetailUrl")
-            article_title = curr_data.get("articleTitle")
-            comment_count = curr_data.get("commentCount")
-            favor_count = curr_data.get("favorCount")
-            nick_name = curr_data.get("nickName")
+        li_list = response.xpath('//div[@aria-label="Navigation menu"]/ul/li')
+        for curr_li in li_list:
+            octree_text = curr_li.xpath("a/text()").get()
+            octree_href = curr_li.xpath("a/@href").get()
 
-            article_item = AyuItem(
-                article_detail_url=article_detail_url,
-                article_title=article_title,
-                comment_count=comment_count,
-                favor_count=favor_count,
-                nick_name=nick_name,
+            octree_item = AyuItem(
+                octree_text=octree_text,
+                octree_href=octree_href,
                 _table="demo_four",
-                _mongo_update_rule={"article_detail_url": article_detail_url},
+                _mongo_update_rule={"octree_text": octree_text},
             )
-            self.slog.info(f"article_item: {article_item}")
-            yield article_item
+            self.slog.info(f"octree_item: {octree_item}")
+            yield octree_item
