5ae4e03e0d193dfdffbe6886908a1381bdd7c639
shengchenyang <15538221825@163.com>
2024-05-17 18:59:03 +0800

    chore: update demo_three

--- DemoSpider/spiders/demo_three.py
+++ DemoSpider/spiders/demo_three.py
@@ -11,8 +11,8 @@ from DemoSpider.common.types import ScrapyResponse
 
 class DemoThreeSpider(AyuSpider):
     name = "demo_three"
-    allowed_domains = ["blog.csdn.net"]
-    start_urls = ["https://blog.csdn.net/"]
+    allowed_domains = ["readthedocs.io"]
+    start_urls = ["https://readthedocs.io"]
     custom_settings = {
         # 开启远程配置服务(优先级 consul > nacos)
         "APP_CONF_MANAGE": True,
@@ -20,43 +20,25 @@ class DemoThreeSpider(AyuSpider):
             # 激活此项则数据会存储至 Mysql
             "ayugespidertools.pipelines.AyuFtyMysqlPipeline": 300,
         },
-        "DOWNLOADER_MIDDLEWARES": {
-            # 随机请求头
-            "ayugespidertools.middlewares.RandomRequestUaMiddleware": 400,
-        },
     }
 
     def start_requests(self) -> Iterable[Request]:
-        """
-        get 请求首页，获取项目列表数据
-        """
         yield Request(
-            url="https://blog.csdn.net/phoenix/web/blog/hot-rank?page=0&pageSize=25&type=",
+            url="https://ayugespidertools.readthedocs.io/en/latest/",
             callback=self.parse_first,
-            headers={
-                "referer": "https://blog.csdn.net/rank/list",
-            },
-            dont_filter=True,
         )
 
     def parse_first(self, response: ScrapyResponse) -> Any:
         _save_table = "demo_three"
+        li_list = response.xpath('//div[@aria-label="Navigation menu"]/ul/li')
+        for curr_li in li_list:
+            octree_text = curr_li.xpath("a/text()").get()
+            octree_href = curr_li.xpath("a/@href").get()
 
-        data_list = json.loads(response.text)["data"]
-        for curr_data in data_list:
-            article_detail_url = curr_data.get("articleDetailUrl")
-            article_title = curr_data.get("articleTitle")
-            comment_count = curr_data.get("commentCount")
-            favor_count = curr_data.get("favorCount")
-            nick_name = curr_data.get("nickName")
-
-            article_item = AyuItem(
-                article_detail_url=article_detail_url,
-                article_title=article_title,
-                comment_count=comment_count,
-                favor_count=favor_count,
-                nick_name=nick_name,
+            octree_item = AyuItem(
+                octree_text=octree_text,
+                octree_href=octree_href,
                 _table=_save_table,
             )
-            self.slog.info(f"article_item: {article_item}")
-            yield article_item
+            self.slog.info(f"octree_item: {octree_item}")
+            yield octree_item
