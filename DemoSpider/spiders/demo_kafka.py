24165d6fdbe74883432825bcbbb85c240f295f6d
shengchenyang <15538221825@163.com>
2024-05-17 19:31:41 +0800

    chore: update demo_kafka

--- DemoSpider/spiders/demo_kafka.py
+++ DemoSpider/spiders/demo_kafka.py
@@ -1,5 +1,4 @@
 # 热榜文章排名采集，并推送至 kafka
-import json
 from typing import Any, Iterable
 
 from ayugespidertools.items import AyuItem
@@ -11,8 +10,8 @@ from DemoSpider.common.types import ScrapyResponse
 
 class DemoKafkaSpider(AyuSpider):
     name = "demo_kafka"
-    allowed_domains = ["csdn.net"]
-    start_urls = ["http://csdn.net/"]
+    allowed_domains = ["readthedocs.io"]
+    start_urls = ["https://readthedocs.io"]
     custom_settings = {
         "ITEM_PIPELINES": {
             "ayugespidertools.pipelines.AyuKafkaPipeline": 301,
@@ -20,33 +19,21 @@ class DemoKafkaSpider(AyuSpider):
     }
 
     def start_requests(self) -> Iterable[Request]:
-        """
-        get 请求首页，获取项目列表数据
-        """
         yield Request(
-            url="https://blog.csdn.net/phoenix/web/blog/hot-rank?page=0&pageSize=25&type=",
+            url="https://ayugespidertools.readthedocs.io/en/latest/",
             callback=self.parse_first,
-            headers={
-                "referer": "https://blog.csdn.net/rank/list",
-            },
         )
 
     def parse_first(self, response: ScrapyResponse) -> Any:
-        data_list = json.loads(response.text)["data"]
-        for curr_data in data_list:
-            article_detail_url = curr_data.get("articleDetailUrl")
-            article_title = curr_data.get("articleTitle")
-            comment_count = curr_data.get("commentCount")
-            favor_count = curr_data.get("favorCount")
-            nick_name = curr_data.get("nickName")
+        li_list = response.xpath('//div[@aria-label="Navigation menu"]/ul/li')
+        for curr_li in li_list:
+            octree_text = curr_li.xpath("a/text()").get()
+            octree_href = curr_li.xpath("a/@href").get()
 
-            article_item = AyuItem(
-                article_detail_url=article_detail_url,
-                article_title=article_title,
-                comment_count=comment_count,
-                favor_count=favor_count,
-                nick_name=nick_name,
+            octree_item = AyuItem(
+                octree_text=octree_text,
+                octree_href=octree_href,
                 _table="demo_kafka",
             )
-            self.slog.info(f"article_item: {article_item}")
-            yield article_item
+            self.slog.info(f"octree_item: {octree_item}")
+            yield octree_item
