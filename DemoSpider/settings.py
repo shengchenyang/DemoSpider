aba28d944e6101bb17a13443a8921b3da7066096
shengchenyang <15538221825@163.com>
2024-10-02 00:34:51 +0800

    chore: clean up the code

--- DemoSpider/settings.py
+++ DemoSpider/settings.py
@@ -9,13 +9,8 @@
 
 from pathlib import Path
 
+from ayugespidertools.config import get_cfg
 from loguru import logger
-
-try:
-    from ayugespidertools.config import get_cfg
-except ImportError:
-    # 目前是预发布功能，所以暂时添加 try catch，正常是不需要的。若你安装了预发布版本也是不用的；
-    get_cfg = None
 
 BOT_NAME = "DemoSpider"
 
@@ -33,19 +28,15 @@ FEED_EXPORT_ENCODING = "utf-8"
 _ = Path(__file__).parent
 VIT_DIR = _ / "VIT"
 
+# Get custom configuration from .conf
 """
-正常情况下这里任何条件判断都不用写，只要 .conf 中确实配置了对应的参数，直接赋值即可；
-这里是因为对预发布功能测试使用，且为了不影响旧项目运行；
-一般直接写成 redis_host = _my_cfg["my_add_redis"].get("host", "localhost") 这样即可获
-取 .conf 中自定义配置，简洁优雅；
-对应的测试请在 spiders/demo_conf.py 中查看。
+这里是在 VIT_DIR .conf 中获取自定义配置，用于隐藏隐私数据和统一配置管理；除了在 settings.py 中获取这
+些配置，当然也可以直接在对应的 spider 中结合 custom_settings 使用。
 """
-if get_cfg:
-    _my_cfg = get_cfg()
-    if _my_cfg.has_section("my_add_redis"):
-        redis_host = _my_cfg["my_add_redis"].get("host", "localhost")
-        redis_port = _my_cfg["my_add_redis"].getint("port", 6379)
-        redis_password = _my_cfg["my_add_redis"].get("password", None)
+_my_cfg = get_cfg()
+redis_host = _my_cfg["my_add_redis"].get("host", "localhost")
+redis_port = _my_cfg["my_add_redis"].getint("port", 6379)
+redis_password = _my_cfg["my_add_redis"].get("password", None)
 
 # Log configuration example
 LOG_LEVEL = "ERROR"
--- DemoSpider/spiders/demo_conf.py
+++ DemoSpider/spiders/demo_conf.py
@@ -4,15 +4,15 @@
 示在 settings.py 里是不可接受的，这里预发布包提供一种功能来解决。
 
 测试介绍：
-测试在 settings.py 中赋值重要参数时，可以从 .conf 中获取自定义配置内容，来达到隐藏隐私内容和
-保持配置内容统一存放的目的；
+测试在 settings.py 中赋值重要参数时，可以从 VIT_DIR 的 .conf 中获取自定义配置内容，来达到隐
+藏隐私内容和保持配置内容统一存放的目的；
 
 1. 介绍本项目中 settings.py 对应的赋值代码为：
 redis_host = _my_cfg["my_add_redis"].get("host", "localhost")
 redis_port = _my_cfg["my_add_redis"].getint("port", 6379)
 redis_password = _my_cfg["my_add_redis"].get("password", None)
 
-2. 项目中 .conf 对应的赋值代码为：
+2. 项目中 .conf 对应的赋值代码示例为：
 [my_add_redis]
 host=localhost
 port=6379
@@ -21,11 +21,8 @@ password=please_write_your_password_here
 你也可以参考 .conf_example 中的示例。
 
 请注意：
-    - 正常情况下，在 settings.py 中导入是不需要 try catch 的，也不用添加任何条件判断语句，这
-    是为了测试预发布版本的功能时，不影响旧项目运行；
-    - 若您安装了预发布包，可尝试清除这些内容，是很优雅简洁的，等后续正式发布时，我会清理这部分；
-    - 功能虽已在预发布阶段，但请不要在正式环境中使用预发布包；安装预发布版本命令如下：
-    pip install git+https://github.com/shengchenyang/AyugeSpiderTools.git
+    - 可以在 settings.py 中使用 get_cfg 方法来获取自定义的隐私配置，也可以在对应的 spider
+     (比如此处)中的 custom_settings 中直接使用，不再举例。
 """
 
 from typing import Any
@@ -33,12 +30,11 @@ from typing import Any
 from ayugespidertools.spiders import AyuSpider
 
 from DemoSpider.common.types import ScrapyResponse
-
-try:
-    from DemoSpider.settings import redis_host, redis_password, redis_port
-except ImportError:
-    # 这里是为了测试预发布版本，且不影响旧项目运行，正常也是不需要 try catch 的。
-    redis_host = redis_port = redis_password = None
+from DemoSpider.settings import (  # 直接从 settings.py 中导入 .conf 中的自定义配置
+    redis_host,
+    redis_password,
+    redis_port,
+)
 
 
 class DemoConfSpider(AyuSpider):
@@ -54,7 +50,8 @@ class DemoConfSpider(AyuSpider):
     def parse(self, response: ScrapyResponse, **kwargs: Any) -> Any:
         if any([redis_host is None, redis_port is None, redis_password is None]):
             self.slog.error(
-                "您应该是没有安装预发布包，或没有在 .conf 中配置 my_add_redis 部分，请参考 .conf_example 的 my_add_redis 部分。"
+                "您安装的 ayugespidertools 版本没有在 3.10.0 及以上，或没有在 .conf 中配置示例所"
+                "需的 my_add_redis 部分，请参考 .conf_example 的 my_add_redis 部分。"
             )
 
         # 正常情况就可以获取到 .conf 中的重要的隐私配置了
