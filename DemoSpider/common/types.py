c2bae8d1239a185c64f00bbc5433b1b92dd36cb5
shengchenyang <15538221825@163.com>
2025-01-15 00:31:15 +0800

    chore: add JsonResponse

--- DemoSpider/common/types.py
+++ DemoSpider/common/types.py
@@ -2,10 +2,11 @@ from typing import Union
 
 from scrapy.http import Response
 from scrapy.http.response.html import HtmlResponse
+from scrapy.http.response.json import JsonResponse
 from scrapy.http.response.text import TextResponse
 from scrapy.http.response.xml import XmlResponse
 
-ScrapyResponse = Union[TextResponse, XmlResponse, HtmlResponse, Response]
+ScrapyResponse = Union[TextResponse, XmlResponse, HtmlResponse, Response, JsonResponse]
 
 
 class RedisSpider: ...
