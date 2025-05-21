9664d29ae969472f1668a2193c7fe2eb379dec51
shengchenyang <15538221825@163.com>
2025-05-21 22:19:25 +0800

    chore: update demo_eleven

--- DemoSpider/spiders/demo_eleven.py
+++ DemoSpider/spiders/demo_eleven.py
@@ -1,11 +1,16 @@
 # postgresql asyncio 场景不会添加自动创建数据库，表及字段等功能，请手动管理
-from typing import Any, Iterable
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
 
 from ayugespidertools.items import AyuItem
 from ayugespidertools.spiders import AyuSpider
 from scrapy.http import Request
 
-from DemoSpider.common.types import ScrapyResponse
+if TYPE_CHECKING:
+    from collections.abc import AsyncIterator
+
+    from DemoSpider.common.types import ScrapyResponse
 
 
 class DemoElevenSpider(AyuSpider):
@@ -13,13 +18,12 @@ class DemoElevenSpider(AyuSpider):
     allowed_domains = ["readthedocs.io"]
     start_urls = ["https://readthedocs.io"]
     custom_settings = {
-        "DATABASE_ENGINE_ENABLED": True,
         "ITEM_PIPELINES": {
             "ayugespidertools.pipelines.AyuAsyncPostgresPipeline": 300,
         },
     }
 
-    def start_requests(self) -> Iterable[Request]:
+    async def start(self) -> AsyncIterator[Any]:
         # 这里请求十次同样 url 是为了测试示例的简单和示例的稳定性，你可自行测试其它目标网站
         for idx, _ in enumerate(range(10)):
             yield Request(
@@ -31,7 +35,6 @@ class DemoElevenSpider(AyuSpider):
 
     def parse_first(self, response: ScrapyResponse, index: str) -> Any:
         _save_table = "demo_eleven"
-
         li_list = response.xpath('//div[@aria-label="Navigation menu"]/ul/li')
         for curr_li in li_list:
             octree_text = curr_li.xpath("a/text()").get()
