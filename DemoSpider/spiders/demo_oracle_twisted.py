02be881b726f7bbbb58ea374b210a2a5c9050a8c
shengchenyang <15538221825@163.com>
2025-05-27 01:00:51 +0800

    chore: update demo_oracle_twisted

--- DemoSpider/spiders/demo_oracle_twisted.py
+++ DemoSpider/spiders/demo_oracle_twisted.py
@@ -1,11 +1,16 @@
 # oracle 场景不会添加自动创建数据库，表及字段等功能，请手动管理
-from typing import Any, Iterable
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
 
 from ayugespidertools.items import AyuItem
 from ayugespidertools.spiders import AyuSpider
 from scrapy.http import Request
 
-from DemoSpider.common.types import ScrapyResponse
+if TYPE_CHECKING:
+    from collections.abc import AsyncIterator
+
+    from DemoSpider.common.types import ScrapyResponse
 
 
 class DemoOracleTwistedSpider(AyuSpider):
@@ -13,7 +18,6 @@ class DemoOracleTwistedSpider(AyuSpider):
     allowed_domains = ["readthedocs.io"]
     start_urls = ["https://readthedocs.io"]
     custom_settings = {
-        "DATABASE_ENGINE_ENABLED": True,
         "ITEM_PIPELINES": {
             "ayugespidertools.pipelines.AyuTwistedOraclePipeline": 300,
         },
@@ -21,7 +25,7 @@ class DemoOracleTwistedSpider(AyuSpider):
         "DOWNLOAD_DELAY": 0.01,
     }
 
-    def start_requests(self) -> Iterable[Request]:
+    async def start(self) -> AsyncIterator[Any]:
         # 这里请求十次同样 url 是为了测试示例的简单和示例的稳定性，你可自行测试其它目标网站
         for idx, _ in enumerate(range(10)):
             yield Request(
