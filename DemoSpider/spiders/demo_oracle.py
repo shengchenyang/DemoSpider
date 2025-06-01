1d7eda1ce927ab38066b189468c04ab142a09156
shengchenyang <15538221825@163.com>
2025-06-02 00:09:10 +0800

    chore: update demo_oracle

--- DemoSpider/spiders/demo_oracle.py
+++ DemoSpider/spiders/demo_oracle.py
@@ -1,26 +1,37 @@
 # oracle 场景不会添加自动创建数据库，表及字段等功能，请手动管理
-from typing import Any, Iterable
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
 
 from ayugespidertools.items import AyuItem
 from ayugespidertools.spiders import AyuSpider
+from ayugespidertools.utils.database import OraclePortal
 from scrapy.http import Request
-from sqlalchemy import text
 
-from DemoSpider.common.types import ScrapyResponse
+if TYPE_CHECKING:
+    from collections.abc import AsyncIterator
+
+    from oracledb import Connection, Cursor
+
+    from DemoSpider.common.types import ScrapyResponse
 
 
 class DemoOracleSpider(AyuSpider):
+    oracle_conn: Connection
+    oracle_cursor: Cursor
+
     name = "demo_oracle"
     allowed_domains = ["readthedocs.io"]
     start_urls = ["https://readthedocs.io"]
     custom_settings = {
-        "DATABASE_ENGINE_ENABLED": True,
         "ITEM_PIPELINES": {
             "ayugespidertools.pipelines.AyuFtyOraclePipeline": 300,
         },
     }
 
-    def start_requests(self) -> Iterable[Request]:
+    async def start(self) -> AsyncIterator[Any]:
+        self.oracle_conn = OraclePortal(db_conf=self.oracle_conf).connect()
+        self.oracle_cursor = self.oracle_conn.cursor()
         yield Request(
             url="https://ayugespidertools.readthedocs.io/en/latest/",
             callback=self.parse_first,
@@ -39,13 +50,11 @@ class DemoOracleSpider(AyuSpider):
                 _table=_save_table,
             )
             self.slog.info(f"octree_item: {octree_item}")
-            # yield octree_item
 
-            if self.oracle_engine_conn:
-                _sql = text(
-                    f"""select 1 from "{_save_table}" where "octree_text" = '{octree_text}' AND ROWNUM <= 1"""
-                )
-                if _ := self.oracle_engine_conn.execute(_sql).fetchone():
-                    self.slog.debug(f'标题为 "{octree_text}" 的数据已存在')
-                else:
-                    yield octree_item
+            sql = f"""select 1 from "{_save_table}" where "octree_text" = '{octree_text}' AND ROWNUM <= 1"""
+            self.oracle_cursor.execute(sql)
+            exists = self.oracle_cursor.fetchone()
+            if exists:
+                self.slog.debug(f'标题为 "{octree_text}" 的数据已存在')
+            else:
+                yield octree_item
