7dc49d90a8f870dd71eff38c25a8e4b3445d70db
shengchenyang <15538221825@163.com>
2026-06-08 00:42:24 +0800

    chore: add demo_curl_cffi

---
+++ DemoSpider/spiders/demo_curl_cffi.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+
+from ayugespidertools.request import CurlCffiRequest
+from ayugespidertools.spiders import AyuSpider
+
+if TYPE_CHECKING:
+    from collections.abc import AsyncIterator
+
+    from scrapy.http.response import Response
+
+
+class DemoCurlCffiSpider(AyuSpider):
+    name = "demo_curl_cffi"
+    allowed_domains = ["browserleaks.com"]
+    start_urls = ["https://tls.browserleaks.com/json"]
+    custom_settings = {
+        "TWISTED_REACTOR": "twisted.internet.asyncioreactor.AsyncioSelectorReactor",
+        "DOWNLOADER_MIDDLEWARES": {
+            # 将 scrapy Request 替换为 curl_cffi 方式
+            "ayugespidertools.middlewares.CurlCffiDownloaderMiddleware": 543,
+        },
+    }
+
+    async def start(self) -> AsyncIterator[Any]:
+        for idx in range(5):
+            yield CurlCffiRequest(
+                url="https://tls.browserleaks.com/json",
+                callback=self.parse_first,
+                cb_kwargs={"request_idx": idx},
+                impersonate="chrome124",
+                dont_filter=True,
+            )
+
+    async def parse_first(self, response: Response, request_idx: int) -> Any:
+        self.slog.info(f"{request_idx} ja3_hash: {response.json()['ja3_hash']}")
